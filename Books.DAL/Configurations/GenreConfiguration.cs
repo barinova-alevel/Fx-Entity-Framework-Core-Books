720e6e9503f1f67d94557a4c69eab2a6eb60d835
barinova-alevel <oksana.barinova12@gmail.com>
2024-09-29 20:23:49 +0300

    Added configurations for models

---
+++ Books.DAL/Configurations/GenreConfiguration.cs
@@ -0,0 +1,16 @@
+﻿
+using Books.DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Books.DataAccessLayer.Configurations
+{
+    public class GenreConfiguration : IEntityTypeConfiguration<Genre>
+    {
+        public void Configure(EntityTypeBuilder<Genre> builder)
+        {
+            builder.ToTable("genres").HasKey(g => g.Id);
+            builder.Property(p => p.Name).IsRequired().HasColumnName("Name");
+        }
+    }
+}
