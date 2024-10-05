6acd386720e9cb8fb89b175b1af56b2ad7a04b80
barinova-alevel <oksana.barinova12@gmail.com>
2024-10-05 21:55:52 +0300

    Not finished setting up

--- Books.DAL/ApplicationContext.cs
+++ Books.DAL/ApplicationContext.cs
@@ -2,27 +2,55 @@
 using Books.DataAccessLayer.Configurations;
 using Books.DataAccessLayer.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace Books.DataAccessLayer
 {
     public class ApplicationContext : DbContext
     {
         public DbSet<Book> Books { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
+
+        public ApplicationContext()
+        { 
+        }
 
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) 
         {
             //  Database.EnsureCreated();
         }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder dBContextOptionsBuilder)
+            => dBContextOptionsBuilder.UseSqlServer("Server=OKSANA_NANGA;Database=BooksDb;Trusted_Connection=True;TrustServerCertificate=True;");
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfiguration(new BookConfiguration());
-            modelBuilder.ApplyConfiguration(new AuthorConfiguration());
-            modelBuilder.ApplyConfiguration(new GenreConfiguration());
-            modelBuilder.ApplyConfiguration(new PublisherConfiguration());
+            modelBuilder.Entity<Book>(entity =>
+            {
+
+                entity.ToTable("Books");
+                entity.HasKey(p => p.Id).HasName("Books");
+
+                entity.Property(p => p.Id)
+
+                .HasColumnName("id")
+
+                .HasColumnType("GUID").ValueGeneratedNever();
+
+                entity.Property(p => p.Title)
+
+                .HasColumnName("title");
+
+            });
+            //modelBuilder.Entity<Book>().HasKey("BookId");
+            //    modelBuilder.ApplyConfiguration(new BookConfiguration());
+            //    modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+            //    modelBuilder.ApplyConfiguration(new GenreConfiguration());
+            //    modelBuilder.ApplyConfiguration(new PublisherConfiguration());
+            //    modelBuilder.ApplyConfiguration(new BookAuthorConfiguration());
         }
     }
 }
--- Books.PL/Program.cs
+++ Books.PL/Program.cs
@@ -6,19 +6,24 @@ class Program
 {
     static void Main()
     {
-        IConfiguration configuration = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json", true, true)
-            .Build();
+        var applicationContext = new ApplicationContext();
+        var book = applicationContext.Books.First();
+        if (book != null)
+        {
+            applicationContext.Books.Remove(book);
+            applicationContext.SaveChanges();
+        }
+        //IConfiguration configuration = new ConfigurationBuilder()
+        //    .AddJsonFile("appsettings.json", true, true)
+        //    .Build();
 
-        var connectionString = configuration.GetConnectionString("DefaultConnection"); //?why null here?
-        var dbOptionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
-        dbOptionsBuilder.UseSqlServer(connectionString, i => i.CommandTimeout(20));
-        dbOptionsBuilder.LogTo(Console.Write);
-        //add logger
+        //var connectionString = configuration.GetConnectionString("DefaultConnection"); //?why null here?
+        //var dbOptionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+        //dbOptionsBuilder.UseSqlServer(connectionString, i => i.CommandTimeout(20));
+        //dbOptionsBuilder.LogTo(Console.Write);
+        ////add logger
 
-        var applicationContext = new ApplicationContext(dbOptionsBuilder.Options);
-        applicationContext.Database.Migrate();
-
-
+        //var applicationContext = new ApplicationContext(dbOptionsBuilder.Options);
+        //applicationContext.Database.Migrate();
     }
 }
