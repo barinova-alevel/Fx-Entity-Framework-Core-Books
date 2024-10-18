811cdcfaef7706ed5272ac54e63f139de8626a4f
barinova-alevel <oksana.barinova12@gmail.com>
2024-10-18 19:38:18 +0300

    Added logger, packages.

--- Books.PL/Program.cs
+++ Books.PL/Program.cs
@@ -1,28 +1,27 @@
 ﻿using Books.DataAccessLayer;
-using Microsoft.EntityFrameworkCore;
+using Books.PresentationLayer;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 
 class Program
 {
     static void Main()
     {
-        //IConfiguration configuration = new ConfigurationBuilder()
-        //    .AddJsonFile("appsettings.json", true, true)
-        //    .Build();
-
-        //var connectionString = configuration.GetConnectionString("DefaultConnection"); //?why null here?
-        //var dbOptionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
-        //dbOptionsBuilder.UseSqlServer(connectionString, i => i.CommandTimeout(20));
-        //dbOptionsBuilder.LogTo(Console.Write);
-        ////add logger
-
-        //var applicationContext = new ApplicationContext(dbOptionsBuilder.Options);
-        //applicationContext.Database.Migrate();
         var applicationContext = new ApplicationContext();
         var book = applicationContext.Books.First();
         applicationContext.SaveChanges();
 
+        IConfigurationRoot builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
 
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder)
+            .CreateLogger();
+        Log.Logger.Information("start");
+
+        InputOutput inputOutput = new InputOutput();
+        //inputOutput.Run();
 
     }
 }
